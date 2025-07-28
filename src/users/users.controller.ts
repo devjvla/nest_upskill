9bc9cecd941877c7a5ac1d7e35fcc8c4a9a9f475
devjvla <dev.jvla@gmail.com>
2025-07-28 14:01:05 +0800

    Setup User controller and service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Patch,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -6,7 +15,30 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  getUsers() {
-    return this.usersService.getUsers();
+  getUsers(@Query('role') role?: 'frontend' | 'backend') {
+    return this.usersService.getUsers(role);
+  }
+
+  @Get(':user_id')
+  getUserById(@Param('user_id') user_id: string) {
+    return this.usersService.getUserById(user_id);
+  }
+
+  @Post()
+  createUser(@Body() user: { first_name: string; last_name: string; email: string }) {
+    return this.usersService.createUser(user);
+  }
+
+  @Patch(':user_id')
+  updateUserById(
+    @Param('user_id') user_id: string,
+    @Body() user: { first_name: string; last_name: string; email: string },
+  ) {
+    return this.usersService.updateUserById({ ...user, user_id });
+  }
+
+  @Delete(':user_id')
+  deleteByUserId(@Param('user_id') user_id: string) {
+    return this.usersService.deleteUserById(user_id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,7 +2,65 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
-  getUsers(): string {
-    return 'getUsers is called.';
+  getUsers(role?: string): string {
+    let return_msg = 'getUsers is called.';
+
+    if (role) {
+      return_msg = 'getUsers is called with role: ' + role;
+    }
+
+    return return_msg;
+  }
+
+  getUserById(user_id: string): string {
+    let return_msg = `getUserById is called with user_id: ${user_id}`;
+
+    // check if user_id is a number
+    try {
+      if (isNaN(Number(user_id))) {
+        throw new Error('user_id must be a number.');
+      }
+    } catch (error) {
+      return_msg = error.message;
+    }
+
+    return return_msg;
+  }
+
+  createUser(user: { first_name: string; last_name: string; email: string}): string {
+    const { first_name, last_name, email } = user;
+
+    return `createUser is called. First Name: ${first_name}, Last Name: ${last_name}, Email: ${email}`;
+  }
+
+  updateUserById(user: { user_id: string; first_name: string; last_name: string; email: string}): string {
+    const { user_id, first_name, last_name, email } = user;
+    let return_msg = `updateUserById is called. User ID: ${user_id}, First Name: ${first_name}, Last Name: ${last_name}, Email: ${email}`;
+
+    // check if user_id is a number
+    try {
+      if (isNaN(Number(user_id))) {
+        throw new Error('user_id must be a number.');
+      }
+    } catch (error) {
+      return_msg = error.message;
+    }
+
+    return return_msg;
+  }
+
+  deleteUserById(user_id: string): string {
+    let return_msg = `deleteUserById is called with user_id: ${user_id}`;
+
+    // check if user_id is a number
+    try {
+      if (isNaN(Number(user_id))) {
+        throw new Error('user_id must be a number.');
+      }
+    } catch (error) {
+      return_msg = error.message;
+    }
+
+    return return_msg;
   }
 }
